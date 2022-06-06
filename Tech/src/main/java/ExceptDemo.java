cd27ec696ae69af71f97018ede213b3154a94e74
jahnavi.siddavaram <jahnavi.siddavaram@DT150154.QICHOAPPL.com>
2022-06-06 11:01:18 +0530

    test

---
+++ Tech/src/main/java/ExceptDemo.java
@@ -0,0 +1,26 @@
+
+public class ExceptDemo {
+	public static void main(String[] args) {
+		if(args.length==0) {
+			System.out.println("pls give a argument value....");
+		}
+		else {
+			String v=args[0];
+			char c=v.charAt(0);
+			if(Character.isDigit(c)) {
+				int n=Integer.parseInt(c+"");
+				if(n==0) {
+					System.out.println("please enter a non zero value...");
+				}
+				else {
+					System.out.println(n);
+					int x=1/n;
+				}
+			}
+			else {
+				System.out.println("please enter number as argument...");
+			}
+			
+		}
+	}
+}
