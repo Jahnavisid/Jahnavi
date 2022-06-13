84f447f7b46fdfbc8db3baf940f59abd46567e9d
jahnavi.siddavaram <jahnavi.siddavaram@DT150154.QICHOAPPL.com>
2022-06-13 15:42:34 +0530

    multi

---
+++ Tech/src/main/java/interjava/ThreadDemo6.java
@@ -0,0 +1,49 @@
+package interjava;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+public class ThreadDemo6 {
+
+	public static void main(String[] args) {
+		ExecutorService es=Executors.newFixedThreadPool(2);
+		es.execute(
+			()->{ {
+				Thread.currentThread().setName("rajini");
+				Theatre.toilet.useToilet();
+			}
+			}
+		);
+		es.execute(()->{synchronized(Theatre.class) {
+			
+			Thread.currentThread().setName("raj");
+			Theatre.toilet.useToilet();
+			
+		}
+		
+		}
+		);
+		es.shutdown();
+		}
+		
+		}
+class Toilet{
+	public void useToilet() {
+		Thread t=Thread.currentThread();
+		String name=t.getName();
+		System.out.println(name+" is using the toilet...");
+		try {
+			Thread.sleep(5000);}catch(Exception e) {
+			}
+		System.out.println(name+" came out of toilet...");
+	}
+}
+		
+		
+		
+	
+class Theatre{
+	static Toilet toilet=new Toilet();
+	
+}
+
+
