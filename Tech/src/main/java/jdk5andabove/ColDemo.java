5c324e42c53aa5dcf39f2232918074d75bb8b14c
jahnavi.siddavaram <jahnavi.siddavaram@DT150154.QICHOAPPL.com>
2022-06-17 14:42:31 +0530

    june17

--- Tech/src/main/java/jdk5andabove/ColDemo.java
+++ Tech/src/main/java/jdk5andabove/ColDemo.java
@@ -1,8 +1,9 @@
 package jdk5andabove;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
-
+import java.util.function.Consumer;
 public class ColDemo {
 
 	public static void main(String[] args) {
@@ -16,9 +17,30 @@ public class ColDemo {
 		System.out.println(hs);
 		System.out.println(hs.isEmpty());
 		System.out.println(hs.contains("baa"));
-		
-		
+		hs.forEach(System.out::println);
+		hs.forEach(new MyConsumer());
+		Iterator<String>it=hs.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
+	
+	static void print(String s) {
+		System.out.println(s);
+	}
+			
+			
+			
+	
+	}
+
+
+class MyConsumer implements Consumer<String>{
+	@Override
+	public void accept(String s) {
+		System.out.println(s);
 		
 	}
 
+	
 }
