662bf5f5295d55ecbb578b8fe1be616d7ffce0cb
jahnavi.siddavaram <jahnavi.siddavaram@DT150154.QICHOAPPL.com>
2022-06-24 15:02:45 +0530

    preparedstatement

---
+++ Tech/src/main/java/jdk5andabove/JDBCDemo3.java
@@ -0,0 +1,34 @@
+package jdk5andabove;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+public class JDBCDemo3 {
+
+	public static void main(String[] args) throws Exception{
+		//Step1 - Load the Driver.
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		//Step 2 - Establish Connection
+		Connection con=DriverManager.getConnection
+				("jdbc:mysql://localhost:3306/tech","root","root");
+		
+		System.out.println(con);
+		//Step 3 - Execute sql statement
+		
+		String sql="update users set flag=? where uname=?";
+		PreparedStatement stmt=con.prepareStatement(sql);
+		
+		stmt.setInt(1,2 );
+		stmt.setInt(2, 2);
+		int noofrowschanged=stmt.executeUpdate();
+		
+		//step 4 - process result
+		
+		System.out.println("No of rows updated...:"+noofrowschanged);
+	}
+
+}
+
+
