79786b8e6db21f91a07bb25c2c3e420051d24cd7
jahnavi.siddavaram <jahnavi.siddavaram@DT150154.QICHOAPPL.com>
2022-06-24 17:16:23 +0530

    june24

--- Tech/src/main/java/jdk5andabove/JDBCDemo5.java
+++ Tech/src/main/java/jdk5andabove/JDBCDemo5.java
@@ -1,10 +1,26 @@
 package jdk5andabove;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class JDBCDemo5 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");;
+		Connection con=DriverManager.getConnection
+				("jdbc:mysql://localhost:3306/tech","root","root");
+       System.out.println(con);
+   	String sql="select * from users";
+	Statement stmt=con.createStatement();
+	
+	ResultSet rs=stmt.executeQuery(sql);
+       while(rs.next()) {
+       System.out.println(rs.getInt("uid")+":"+rs.getString("uname"));
 	}
 
+	}
 }
+
+   
